979d9587df0bf007fd9eef90fb69fdc3536ce8d4
macierzynski-203932 <[203932@edu.p.lodz.pl]>
2018-06-15 17:14:43 +0200

    lol

---
+++ backend/src/test/java/pl/mrs/movie_raiting_system/converters/UserInfoConverterTest.java
@@ -0,0 +1,13 @@
+package pl.mrs.movie_raiting_system.converters;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserInfoConverterTest {
+    @Test
+    public void shouldnotpassed() {
+        int i =0;
+        assertEquals(1,i);
+    }
+}
